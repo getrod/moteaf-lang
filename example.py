d3921e9ab59680507f3fc33edc3ec936a22bfa87
getrod <kevinlouisjean@gmail.com>
2022-12-31 16:16:50 -0500

    fix example.py

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from moteaf import motif_parse, motif_2_midi
+from moteaf import motif_parse, motif_2_midi, save_midi
 
 motif ="""
 Am9[0 1 2 3 4 :: 4]-(3/2), 
@@ -8,4 +8,6 @@ Em7<[3 5]-(3/4), [6]-(1/4), [7 9]-(1/4), [8]-(1/4), [6]-(1/4) :: 4>,
 Em7<[3]-(1/4), [2]-(1/4), [3]-(1/4), [4]-(1/4), [1]-(1/4) :: 4>
 """
 
-motif_2_midi(motif, 'ballin.mid')
+motif_tree = motif_parse(motif)
+midi = motif_2_midi(motif_tree)
+save_midi(midi, 'ballin.mid')
